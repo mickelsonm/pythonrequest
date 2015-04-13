f0ed81c9cfa0a3f2e2af47863e81420623db4569
Matt Mickelson <mickelmr@gmail.com>
2015-04-12 23:56:25 -0500

    add a value error exception error case

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ def main():
 		res = requests.get('http://www.google.com')
 		res.raise_for_status()
 		print res.text
+
+		#if it is json...
+		data = res.json()
+		print data
+	except ValueError as e:
+		#catches value errors..example, if we wanted json and it couldn't decode it
+		print "Value Error: ", e.message
 	except requests.exceptions.HTTPError as e:
 		 print "HTTP Error:", e.message
 	except requests.exceptions.ConnectionError as e:
